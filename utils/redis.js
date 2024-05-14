dc25177cea846b2195c4f8d984d11ca1951fe9c0
Mefdev <mefdev.pro@gmail.com>
2024-05-14 22:18:16 +0100

    Create utils

---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+import { createClient } from 'redis';
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (error) => {
+            console.error(error);
+        }); 
+    }
+    
+    isAlive() {
+        return this.client.connected;
+    }
+    async get(key){
+        new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if(err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+    async set(key, value, duration){
+        new Promise((resolve, reject) => {
+            this.client.set(key, value, (err, reply) => {
+                if(err) reject(err);
+                resolve(reply);
+            });
+            this.client.expire(key, duration);
+        });
+    }  
+    del(key){
+        new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if(err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+    }
+
+ 
+
+const redisClient = new RedisClient();
+export default redisClient;
+
